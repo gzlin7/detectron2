83b2942c8443db1e2c93f7180d96c40429b126e3
Emma Liu <emmaliu@mit.edu>
2021-05-12 22:29:20 -0400

    Remove boundary fusion

--- projects/ContextualFusion/bmaskrcnn/mask_head.py
+++ projects/ContextualFusion/bmaskrcnn/mask_head.py
@@ -136,12 +136,97 @@ def boundary_preserving_mask_loss(
     boundary_loss = boundary_loss_func(pred_boundary_logits, gt_masks)
     return mask_loss, boundary_loss
 
+@torch.jit.unused
+def mask_rcnn_loss(pred_mask_logits: torch.Tensor, instances: List[Instances], vis_period: int = 0):
+    """
+    Compute the mask prediction loss defined in the Mask R-CNN paper.
+
+    Args:
+        pred_mask_logits (Tensor): A tensor of shape (B, C, Hmask, Wmask) or (B, 1, Hmask, Wmask)
+            for class-specific or class-agnostic, where B is the total number of predicted masks
+            in all images, C is the number of foreground classes, and Hmask, Wmask are the height
+            and width of the mask predictions. The values are logits.
+        instances (list[Instances]): A list of N Instances, where N is the number of images
+            in the batch. These instances are in 1:1
+            correspondence with the pred_mask_logits. The ground-truth labels (class, box, mask,
+            ...) associated with each instance are stored in fields.
+        vis_period (int): the period (in steps) to dump visualization.
+
+    Returns:
+        mask_loss (Tensor): A scalar tensor containing the loss.
+    """
+    cls_agnostic_mask = pred_mask_logits.size(1) == 1
+    total_num_masks = pred_mask_logits.size(0)
+    mask_side_len = pred_mask_logits.size(2)
+    assert pred_mask_logits.size(2) == pred_mask_logits.size(3), "Mask prediction must be square!"
+
+    gt_classes = []
+    gt_masks = []
+    for instances_per_image in instances:
+        if len(instances_per_image) == 0:
+            continue
+        if not cls_agnostic_mask:
+            gt_classes_per_image = instances_per_image.gt_classes.to(dtype=torch.int64)
+            gt_classes.append(gt_classes_per_image)
+
+        gt_masks_per_image = instances_per_image.gt_masks.crop_and_resize(
+            instances_per_image.proposal_boxes.tensor, mask_side_len
+        ).to(device=pred_mask_logits.device)
+        # A tensor of shape (N, M, M), N=#instances in the image; M=mask_side_len
+        gt_masks.append(gt_masks_per_image)
+
+    if len(gt_masks) == 0:
+        return pred_mask_logits.sum() * 0
+
+    gt_masks = cat(gt_masks, dim=0)
+
+    if cls_agnostic_mask:
+        pred_mask_logits = pred_mask_logits[:, 0]
+    else:
+        indices = torch.arange(total_num_masks)
+        gt_classes = cat(gt_classes, dim=0)
+        pred_mask_logits = pred_mask_logits[indices, gt_classes]
+
+    if gt_masks.dtype == torch.bool:
+        gt_masks_bool = gt_masks
+    else:
+        # Here we allow gt_masks to be float as well (depend on the implementation of rasterize())
+        gt_masks_bool = gt_masks > 0.5
+    gt_masks = gt_masks.to(dtype=torch.float32)
+
+    # Log the training accuracy (using gt classes and 0.5 threshold)
+    mask_incorrect = (pred_mask_logits > 0.0) != gt_masks_bool
+    mask_accuracy = 1 - (mask_incorrect.sum().item() / max(mask_incorrect.numel(), 1.0))
+    num_positive = gt_masks_bool.sum().item()
+    false_positive = (mask_incorrect & ~gt_masks_bool).sum().item() / max(
+        gt_masks_bool.numel() - num_positive, 1.0
+    )
+    false_negative = (mask_incorrect & gt_masks_bool).sum().item() / max(num_positive, 1.0)
+
+    storage = get_event_storage()
+    storage.put_scalar("mask_rcnn/accuracy", mask_accuracy)
+    storage.put_scalar("mask_rcnn/false_positive", false_positive)
+    storage.put_scalar("mask_rcnn/false_negative", false_negative)
+    if vis_period > 0 and storage.iter % vis_period == 0:
+        pred_masks = pred_mask_logits.sigmoid()
+        vis_masks = torch.cat([pred_masks, gt_masks], axis=2)
+        name = "Left: mask prediction;   Right: mask GT"
+        for idx, vis_mask in enumerate(vis_masks):
+            vis_mask = torch.stack([vis_mask] * 3, axis=0)
+            storage.put_image(name + f" ({idx})", vis_mask)
+
+    mask_loss = F.binary_cross_entropy_with_logits(pred_mask_logits, gt_masks, reduction="mean")
+    return mask_loss
+
 
 @ROI_MASK_HEAD_REGISTRY.register()
 class ContextualFusionHead(nn.Module):
 
-    def __init__(self, cfg, input_shape: ShapeSpec):
+    def __init__(self, cfg, input_shape: ShapeSpec, vis_period: int = 0, loss_weight: float = 1.0):
         super(ContextualFusionHead, self).__init__()
+
+        self.vis_period = vis_period
+        self.loss_weight = loss_weight
 
         conv_dim = cfg.MODEL.ROI_MASK_HEAD.CONV_DIM
         num_conv = cfg.MODEL.ROI_MASK_HEAD.NUM_CONV
@@ -245,29 +330,24 @@ class ContextualFusionHead(nn.Module):
         if self.boundary_predictor.bias is not None:
             nn.init.constant_(self.boundary_predictor.bias, 0)
 
-    def forward(self, mask_features, boundary_features, instances: List[Instances]):
+    def forward(self, mask_features, context_feature, instances: List[Instances]):
         for layer in self.mask_fcns:
             mask_features = layer(mask_features)
         # downsample
-        boundary_features = self.downsample(boundary_features)
+        context_feature = self.downsample(context_feature)
         # mask to boundary fusion
-        boundary_features = boundary_features + self.mask_to_boundary(mask_features)
+        # context_feature = context_feature + self.mask_to_boundary(mask_features)
         for layer in self.boundary_fcns:
-            boundary_features = layer(boundary_features)
+            context_feature = layer(context_feature)
         # boundary to mask fusion
-        mask_features = self.boundary_to_mask(boundary_features) + mask_features
+        mask_features = self.boundary_to_mask(context_feature) + mask_features
         mask_features = self.mask_final_fusion(mask_features)
         # mask prediction
         mask_features = F.relu(self.mask_deconv(mask_features))
         mask_logits = self.mask_predictor(mask_features)
-        # boundary prediction
-        boundary_features = F.relu(self.boundary_deconv(boundary_features))
-        boundary_logits = self.boundary_predictor(boundary_features)
+
         if self.training:
-            loss_mask, loss_boundary = boundary_preserving_mask_loss(
-                mask_logits, boundary_logits, instances)
-            return {"loss_mask": loss_mask,
-                    "loss_boundary": loss_boundary}
+            return {"loss_mask": mask_rcnn_loss(mask_logits, instances, self.vis_period) * self.loss_weight}
         else:
             mask_rcnn_inference(mask_logits, instances)
             return instances
