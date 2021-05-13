d772cf7bdf4b195275521fc87017850727e2ec38
Emma Liu <emmaliu@mit.edu>
2021-05-12 22:09:58 -0400

    Updated init

--- projects/ContextualFusion/bmaskrcnn/__init__.py
+++ projects/ContextualFusion/bmaskrcnn/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) wondervictor. All Rights Reserved
 from .config import add_boundary_preserving_config
-from .mask_head import BoundaryPreservingHead
-from .roi_heads import BoundaryROIHeads
+from .mask_head import ContextualFusionHead
+from .roi_heads import ContextualFusionROIHeads
 from .cascade_rcnn import CascadeBoundaryROIHeads
