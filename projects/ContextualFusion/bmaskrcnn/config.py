dde6a07bb95a0c651ced5ee76bc8f86a35c69cc8
Emma Liu <emmaliu@mit.edu>
2021-05-12 22:41:35 -0400

    remove boundary

--- projects/ContextualFusion/bmaskrcnn/config.py
+++ projects/ContextualFusion/bmaskrcnn/config.py
@@ -5,8 +5,8 @@ from detectron2.config import CfgNode as CN
 
 def add_boundary_preserving_config(cfg):
 
-    cfg.MODEL.BOUNDARY_MASK_HEAD = CN()
-    cfg.MODEL.BOUNDARY_MASK_HEAD.POOLER_RESOLUTION = 28
-    cfg.MODEL.BOUNDARY_MASK_HEAD.IN_FEATURES = ("p2",)
-    cfg.MODEL.BOUNDARY_MASK_HEAD.NUM_CONV = 2
+    cfg.MODEL.CONTEXT_MASK_HEAD = CN()
+    cfg.MODEL.CONTEXT_MASK_HEAD.POOLER_RESOLUTION = 28
+    cfg.MODEL.CONTEXT_MASK_HEAD.IN_FEATURES = ("p2",)
+    cfg.MODEL.CONTEXT_MASK_HEAD.NUM_CONV = 2
 
