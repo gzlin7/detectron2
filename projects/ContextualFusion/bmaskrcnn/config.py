2c7ce20b56c8c43aa2025d8a3a2bec5da2957b98
Emma Liu <emmaliu@mit.edu>
2021-05-14 10:51:13 -0400

    Updated config

--- projects/ContextualFusion/bmaskrcnn/config.py
+++ projects/ContextualFusion/bmaskrcnn/config.py
@@ -7,6 +7,6 @@ def add_context_preserving_config(cfg):
 
     cfg.MODEL.CONTEXT_MASK_HEAD = CN()
     cfg.MODEL.CONTEXT_MASK_HEAD.POOLER_RESOLUTION = 28
-    cfg.MODEL.CONTEXT_MASK_HEAD.IN_FEATURES = ("p2",)
+    cfg.MODEL.CONTEXT_MASK_HEAD.IN_FEATURES = ("p3",)
     cfg.MODEL.CONTEXT_MASK_HEAD.NUM_CONV = 2
 
